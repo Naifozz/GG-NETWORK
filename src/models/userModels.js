694e15b9c7ffd422b7f11cea977080809f29a89d
Idazgit <tristanctxecole@gmail.com>
2025-03-28 10:31:07 +0100

    feat(usermodels): added the user model with fields for name / change to unique nom in schemaprisma
    create the vérification for the name of user in usermodels and change to unique nom in schemaprisma,
    create index un utilisateur table for nom

---
+++ src/models/userModels.js
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+import prisma from './prisma';
+
+export const utilisateurSchema = z.object({
+  nom: z
+    .string()
+    .min(3, {
+      message: "Le nom d'utilisateur doit avoir au moins 3 caractères",
+    })
+    .max(50, {
+      message: "Le nom d'utilisateur ne peut pas dépasser 50 caractères",
+    })
+    .toLowerCase() // Convertit automatiquement en minuscules
+    .regex(/^[a-z0-9_]+$/, {
+      message:
+        "Le nom d'utilisateur ne peut contenir que des lettres minuscules, des chiffres et des underscores",
+    }),
+});
+
+export const validateUtilisateur = async (utilisateur) => {
+  const erreur = [];
+  try {
+    utilisateurSchema.parse(utilisateur);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      erreur.push(...error.errors.map((err) => err.message));
+    }
+  }
+
+  const utilisateurExistant = await prisma.utilisateur.findUnique({
+    where: { nom: utilisateur.nom.toLowerCase() },
+  });
+  if (utilisateurExistant) {
+    erreur.push("Ce nom d'utilisateur est déjà utilisé");
+  }
+  return erreur;
+};
