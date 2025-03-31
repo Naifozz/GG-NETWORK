e7b27f62c0afe67fd316fb32962ca212e9038924
LgTechh <valentinvanhelle@hotmail.fr>
2025-03-31 11:58:18 +0200

    test(profilService): add tests for profilService methods

---
+++ tests/profil/services/profilService.test.js
@@ -0,0 +1,171 @@
+import { describe, test, expect, vi, beforeEach } from 'vitest';
+import * as profilRepository from '../../../src/repositories/profilRepository.js';
+import { ProfilService } from '../../../src/services/ProfilService.js';
+
+vi.mock('../repository/Profil', () => ({
+  getAllProfils: vi.fn(),
+  createProfil: vi.fn(),
+  getProfilById: vi.fn(),
+  updateProfil: vi.fn(),
+  deleteProfil: vi.fn(),
+}));
+
+describe('ProfilService', () => {
+  let service;
+
+  beforeEach(() => {
+    service = new ProfilService();
+    vi.clearAllMocks();
+  });
+
+  //Verif de l'execution du repo renvoyée
+  test('✅ getAllProfils devrait retourner tous les profils', async () => {
+    const mockProfils = [
+      {
+        ID_Profil: 1,
+        Description: 'Profil 1',
+        Profil_Privacy: true,
+        Statut: 'Actif',
+      },
+      {
+        ID_Profil: 2,
+        Description: 'Profil 2',
+        Profil_Privacy: false,
+        Statut: 'Inactif',
+      },
+    ];
+
+    profilRepository.getAllProfils.mockResolvedValue(mockProfils);
+
+    const result = await service.getAllProfils();
+
+    expect(result).toEqual(mockProfils);
+    expect(profilRepository.getAllProfils).toHaveBeenCalledTimes(1);
+  });
+
+  //Verif de l'erreur renvoyée
+  test('❌ getAllProfils devrait gérer les erreurs correctement', async () => {
+    const errorMessage = 'Erreur lors de la récupération des profils';
+
+    profilRepository.getAllProfils.mockRejectedValue(new Error(errorMessage));
+
+    try {
+      await service.getAllProfils();
+    } catch (error) {
+      expect(error.message).toBe(errorMessage);
+      expect(profilRepository.getAllProfils).toHaveBeenCalledTimes(1);
+    }
+  });
+
+  test('✅ createProfil devrait retourner le profil créé', async () => {
+    const newProfil = {
+      Description: 'New Profil',
+      ID_Connexion: 'New Profil',
+      Profil_Privacy: true,
+      Statut: 'Actif',
+    };
+    const createdProfil = { ID_Profil: 3, ...newProfil };
+
+    profilRepository.createProfil.mockResolvedValue(createdProfil);
+
+    const result = await service.createProfil(newProfil);
+
+    expect(result).toEqual(createdProfil);
+    expect(profilRepository.createProfil).toHaveBeenCalledWith(newProfil);
+  });
+
+  test('❌ createProfil devrait gérer les erreurs correctement', async () => {
+    const errorMessage = 'Erreur lors de la création du profil';
+
+    profilRepository.createProfil.mockRejectedValue(new Error(errorMessage));
+
+    try {
+      await service.createProfil();
+    } catch (error) {
+      expect(error.message).toBe(errorMessage);
+      expect(profilRepository.createProfil).toHaveBeenCalledTimes(1);
+    }
+  });
+
+  test('✅ getProfilById devrais retourner le profil correspondant', async () => {
+    const profil = {
+      ID_Profil: 1,
+      Description: 'Profil 1',
+      Profil_Privacy: true,
+      Statut: 'Actif',
+    };
+
+    const result = await service.getProfilById.mockResolvedValue(profil);
+
+    expect(result).toEqual(profil);
+    expect(profilRepository.getProfilById).toHaveBeenCalledWith(1);
+  });
+
+  test('❌ getProfilById devrait gérer les erreurs correctement', async () => {
+    const errorMessage =
+      'Erreur lors de la récupération du profil correspondant';
+
+    profilRepository.getProfilById.mockRejectedValue(new Error(errorMessage));
+
+    try {
+      await service.getProfilById();
+    } catch (error) {
+      expect(error.message).toBe(errorMessage);
+      expect(profilRepository.getProfilById).toHaveBeenCalledTimes(1);
+    }
+  });
+
+  test('✅ updateProfil devrais retourner le profil modifié', async () => {
+    const updatedData = { Description: 'Updated Profil' };
+    const updatedProfil = {
+      ID_Profil: 1,
+      ...updatedData,
+      Profil_Privacy: true,
+      Statut: 'Actif',
+    };
+
+    profilRepository.updateProfil.mockResolvedValue(updatedProfil);
+
+    const result = await service.updateProfil(1, updatedData);
+
+    expect(result).toEqual(updatedProfil);
+    expect(profilRepository.updateProfil).toHaveBeenCalledWith(1, updatedData);
+  });
+
+  test('❌ updateProfil devrait gérer les erreurs correctement', async () => {
+    const errorMessage =
+      'Erreur lors de la modification du profil correspondant';
+
+    profilRepository.updateProfil.mockRejectedValue(new Error(errorMessage));
+
+    try {
+      await service.updateProfil();
+    } catch (error) {
+      expect(error.message).toBe(errorMessage);
+      expect(profilRepository.updateProfil).toHaveBeenCalledTimes(1);
+    }
+  });
+
+  test('✅ deleteProfil devrait supprimer un profil', async () => {
+    profilRepository.deleteProfil.mockResolvedValue({ ID_Profil: 1 });
+
+    const result = await service.deleteProfil(1);
+
+    expect(result).toEqual({ ID_Profil: 1 });
+    expect(profilRepository.deleteProfil).toHaveBeenCalledWith(1);
+  });
+
+  test('❌ deleteProfil devrait gérer les erreurs correctement', async () => {
+    const errorMessage =
+      'Erreur lors de la suppression du profil correspondant';
+
+    profilRepository.deleteProfil.mockRejectedValue(new Error(errorMessage));
+
+    try {
+      await service.deleteProfil();
+    } catch (error) {
+      expect(error.message).toBe(errorMessage);
+      expect(profilRepository.deleteProfil).toHaveBeenCalledTimes(1);
+    }
+  });
+});
