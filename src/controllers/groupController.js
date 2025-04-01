18f39334934cb5abec6c8ded23c5c3c650574d82
Naifozz <sdebraeckelaer@gmail.com>
2025-04-01 14:12:59 +0200

    fix(groupcontroller): corrected id
    it was taking the id as a string and not a int

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -32,7 +32,7 @@ export const getGroupById = async (req, res) => {
 export const createGroup = async (req, res) => {
   try {
     const group = await groupService.createGroup(req.body);
-    res.status(200).json(group);
+    res.status(201).json(group);
   } catch (error) {
     console.error('Error creating group', error);
     res.status(500).json({ message: 'Erreur lors de la création du groupe' });
