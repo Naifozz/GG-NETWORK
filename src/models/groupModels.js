c9873d9f99d8dcc90addc3d8861ece5fb8c98dde
Naifozz <sdebraeckelaer@gmail.com>
2025-04-01 09:04:54 +0200

    fix(groupmodels): made validateArticle async

--- src/models/groupModels.js
+++ src/models/groupModels.js
@@ -3,7 +3,7 @@ import { z } from 'zod';
 // Schéma de validation  d'un groupe
 const groupeSchema = z.object({
   // Nom du groupe
-  nom: z
+  Nom: z
     .string()
     .min(2, { message: 'Le nom du groupe doit contenir au moins 2 caractères' })
     .max(100, {
@@ -12,30 +12,33 @@ const groupeSchema = z.object({
     .trim(),
 
   // Description optionnelle
-  description: z
+  Description: z
     .string()
     .max(1000, {
       message: 'La description ne peut pas dépasser 1000 caractères',
     })
     .optional(),
 
-  // Etat du groupe: Public ou Prive
-  etat: z
-    .enum(['Public', 'Prive'], {
+  // Etat du groupe: true (Public) ou false (Privé)
+  Etat: z
+    .boolean({
       errorMap: () => ({
-        message: 'L\'état du groupe doit être "Public" ou "Prive"',
+        message:
+          "L'état du groupe doit être un boolean (true pour Public, false pour Privé)",
       }),
     })
-    .default('Public'),
+    .default(true), // Par défaut, l'état est "Public" (true)
 });
 
-export const validateGroupe = (data) => {
+export const validateGroupe = async (data) => {
   const result = groupeSchema.safeParse(data);
   if (!result.success) {
+    console.error('Validation errors:', result.error.issues);
     // Concaténer les messages d'erreur (ou choisir le premier) pour être retourné
     const errorMessage = result.error.issues
       .map((issue) => issue.message)
       .join(', ');
+
     throw new Error(errorMessage);
   }
   return result.data;
