e32a8bf7fd7cb59d9947c391125a0cae8588be93
Idazgit <tristanctxecole@gmail.com>
2025-04-01 10:17:22 +0200

    feat(models): update models for pseudo and email

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -10,17 +10,37 @@ export const utilisateurSchema = z.object({
     .max(50, {
       message: "Le nom d'utilisateur ne peut pas dépasser 50 caractères",
     })
-    .toLowerCase()
     .regex(/^[a-z0-9_]+$/, {
       message:
         "Le nom d'utilisateur ne peut contenir que des lettres minuscules, des chiffres et des underscores",
     }),
+
+  pseudo: z
+    .string()
+    .min(3, {
+      message: 'Le pseudo doit avoir au moins 3 caractères',
+    })
+    .max(25, {
+      message: 'Le pseudo ne peut pas dépasser 25 caractères',
+    })
+    .regex(/^[a-zA-Z0-9@!_.-]+$/, {
+      message:
+        'Le pseudo ne peut contenir que des lettres, chiffres et certains caractères spéciaux (@, !, _, ., -)',
+    }),
+
+  email: z
+    .string()
+    .email({ message: "L'email n'est pas valide" })
+    .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+      message: "L'email doit être valide (ex: exemple@gaming.com)",
+    }),
 });
 
-export const validateUtilisateur = async (utilisateur) => {
+export const validateUtilisateur = async (Utilisateur) => {
+  console.log('Prisma dans validateUtilisateur:', prisma);
   const erreur = [];
   try {
-    utilisateurSchema.parse(utilisateur);
+    utilisateurSchema.parse(Utilisateur);
   } catch (error) {
     if (error instanceof z.ZodError) {
       // N'extraire que les messages
@@ -28,11 +48,22 @@ export const validateUtilisateur = async (utilisateur) => {
     }
   }
 
-  const utilisateurExistant = await prisma.utilisateur.findUnique({
-    where: { nom: utilisateur.nom.toLowerCase() },
-  });
+  const [utilisateurExistant, emailExistant] = await Promise.all([
+    prisma.Utilisateur.findUnique({
+      where: { nom: Utilisateur.nom },
+    }),
+    prisma.Utilisateur.findUnique({
+      where: { email: Utilisateur.email },
+    }),
+  ]);
+  console.log('Utilisateur existant:', utilisateurExistant);
+  console.log('Email existant:', emailExistant);
+
   if (utilisateurExistant) {
     erreur.push("Ce nom d'utilisateur est déjà utilisé");
+  }
+  if (emailExistant) {
+    erreur.push('Cet email est déjà utilisé');
   }
   return erreur;
 };
