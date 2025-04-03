911406eb54ad506e4c64b1f18040954eb97d3fb6
Naifozz <sdebraeckelaer@gmail.com>
2025-04-03 13:24:34 +0200

    test(usergrouprepository): fixed update test

--- tests/groupTests/repositories/userGroupRepository.test.js
+++ tests/groupTests/repositories/userGroupRepository.test.js
@@ -9,6 +9,7 @@ vi.mock('@prisma/client', () => {
       findMany: vi.fn(),
       findUnique: vi.fn(),
       create: vi.fn(),
+      update: vi.fn(),
       delete: vi.fn(),
       deleteMany: vi.fn(),
     },
@@ -25,8 +26,8 @@ afterEach(() => {
 describe('Tests unitaires pour userGroupRepository', () => {
   test('✅ Récupération de tous les UserGroupes', async () => {
     const mockUserGroups = [
-      { ID_UserGroupe: 1, ID_Utilisateur: 1, ID_Groupe: 1, IsMod: false },
-      { ID_UserGroupe: 2, ID_Utilisateur: 2, ID_Groupe: 1, IsMod: true },
+      { ID_Utilisateur: 1, ID_Group: 1, IsMod: false },
+      { ID_Utilisateur: 2, ID_Group: 1, IsMod: true },
     ];
     prisma.userGroupe.findMany.mockResolvedValue(mockUserGroups);
 
@@ -36,9 +37,7 @@ describe('Tests unitaires pour userGroupRepository', () => {
   });
 
   test('✅ Récupération des UserGroupes par ID_Utilisateur', async () => {
-    const mockUserGroups = [
-      { ID_UserGroupe: 1, ID_Utilisateur: 1, ID_Groupe: 1, IsMod: false },
-    ];
+    const mockUserGroups = [{ ID_Utilisateur: 1, ID_Group: 1, IsMod: false }];
     prisma.userGroupe.findMany.mockResolvedValue(mockUserGroups);
 
     const result = await userGroupRepository.getUserGroupsByUserId(1);
@@ -49,9 +48,7 @@ describe('Tests unitaires pour userGroupRepository', () => {
   });
 
   test('✅ Récupération des UserGroupes par ID_Groupe', async () => {
-    const mockUserGroups = [
-      { ID_UserGroupe: 1, ID_Utilisateur: 1, ID_Groupe: 1, IsMod: false },
-    ];
+    const mockUserGroups = [{ ID_Utilisateur: 1, ID_Group: 1, IsMod: false }];
     prisma.userGroupe.findMany.mockResolvedValue(mockUserGroups);
 
     const result = await userGroupRepository.getUserGroupsByGroupId(1);
@@ -63,33 +60,55 @@ describe('Tests unitaires pour userGroupRepository', () => {
 
   test("✅ Création d'un UserGroupe", async () => {
     const mockUserGroup = {
-      ID_UserGroupe: 1,
       ID_Utilisateur: 1,
-      ID_Groupe: 1,
+      ID_Group: 1,
       IsMod: false,
     };
     prisma.userGroupe.create.mockResolvedValue(mockUserGroup);
 
-    const data = { ID_Utilisateur: 1, ID_Groupe: 1, IsMod: false };
+    const data = { ID_Utilisateur: 1, ID_Group: 1, IsMod: false };
     const result = await userGroupRepository.createUserGroup(data);
     expect(prisma.userGroupe.create).toHaveBeenCalledWith({ data });
     expect(result).toEqual(mockUserGroup);
   });
 
-  test("✅ Suppression d'un UserGroupe par ID_UserGroupe", async () => {
-    const mockUserGroup = {
-      ID_UserGroupe: 1,
+  test("✅ Mise à jour d'un UserGroupe", async () => {
+    const mockUpdatedUserGroup = {
       ID_Utilisateur: 1,
-      ID_Groupe: 1,
+      ID_Group: 1,
+      IsMod: true,
+    };
+    prisma.userGroupe.update.mockResolvedValue(mockUpdatedUserGroup);
+
+    const data = { ID_Utilisateur: 1, ID_Group: 1, IsMod: true };
+    const result = await userGroupRepository.updateUserGroup(data);
+    expect(prisma.userGroupe.update).toHaveBeenCalledWith({
+      where: {
+        ID_Utilisateur_ID_Group: {
+          ID_Utilisateur: 1,
+          ID_Group: 1,
+        },
+      },
+      data: {
+        IsMod: true,
+      },
+    });
+    expect(result).toEqual(mockUpdatedUserGroup);
+  });
+
+  test("✅ Suppression d'un UserGroupe par clé composite", async () => {
+    const mockDeletedUserGroup = {
+      ID_Utilisateur: 1,
+      ID_Group: 1,
       IsMod: false,
     };
-    prisma.userGroupe.delete.mockResolvedValue(mockUserGroup);
+    prisma.userGroupe.delete.mockResolvedValue(mockDeletedUserGroup);
 
     const result = await userGroupRepository.deleteUserGroupById(1);
     expect(prisma.userGroupe.delete).toHaveBeenCalledWith({
       where: { ID_UserGroupe: 1 },
     });
-    expect(result).toEqual(mockUserGroup);
+    expect(result).toEqual(mockDeletedUserGroup);
   });
 
   test('✅ Suppression de plusieurs UserGroupes par ID_Groupe', async () => {
@@ -98,7 +117,7 @@ describe('Tests unitaires pour userGroupRepository', () => {
 
     const result = await userGroupRepository.deleteUserGroupByGroupId(1);
     expect(prisma.userGroupe.deleteMany).toHaveBeenCalledWith({
-      where: { ID_Groupe: 1 },
+      where: { ID_Group: 1 },
     });
     expect(result).toEqual(mockDeleteResult);
   });
