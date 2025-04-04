1c32a2a7fa3226832c505543389c0f005fa0d25f
Naifozz <sdebraeckelaer@gmail.com>
2025-04-04 09:10:12 +0200

    fix(groupservice): added usergroup repository functions

--- src/services/groupService.js
+++ src/services/groupService.js
@@ -1,5 +1,6 @@
 import * as groupRepository from '../repositories/groupRepository.js';
 import { validateGroupe } from '../models/groupModels.js';
+import * as userGroupRepository from '../repositories/userGroupRepository.js';
 
 export const getGroups = async () => {
   try {
@@ -29,6 +30,13 @@ export const createGroup = async (data) => {
     const validatedGroupe = await validateGroupe(data);
 
     const group = await groupRepository.createGroup(validatedGroupe);
+
+    await userGroupRepository.createUserGroup({
+      ID_Utilisateur: validatedGroupe.ID_Utilisateur,
+      ID_Group: group.ID_Group,
+      IsMod: true,
+    });
+
     return group;
   } catch (error) {
     console.error('Error creating group', error);
@@ -38,12 +46,38 @@ export const createGroup = async (data) => {
 
 export const updateGroup = async (id, data) => {
   try {
-    const existanceGroup = await groupRepository.getGroupById(id);
-    if (!existanceGroup) {
+    const existingGroup = await groupRepository.getGroupById(id);
+    if (!existingGroup) {
       throw { status: 404, message: `Groupe avec l'ID ${id} introuvable` };
     }
 
     const validatedGroupe = await validateGroupe(data);
+
+    if (existingGroup.ID_Utilisateur !== validatedGroupe.ID_Utilisateur) {
+      // Vérifier si l'ancien utilisateur est un modérateur
+      const userGroups = await userGroupRepository.getUserGroupsByUserId(
+        existingGroup.ID_Utilisateur,
+      );
+      const isOldUserMod = userGroups.some(
+        (ug) => ug.ID_Group === id && ug.IsMod === true,
+      );
+
+      if (isOldUserMod) {
+        // Retirer les droits de modérateur à l'ancien utilisateur
+        await userGroupRepository.updateUserGroup({
+          ID_Utilisateur: existingGroup.ID_Utilisateur,
+          ID_Group: id,
+          IsMod: false,
+        });
+      }
+
+      // Ajouter les droits de modérateur au nouvel utilisateur
+      await userGroupRepository.createUserGroup({
+        ID_Utilisateur: validatedGroupe.ID_Utilisateur,
+        ID_Group: id,
+        IsMod: true,
+      });
+    }
     const group = await groupRepository.updateGroup(id, validatedGroupe);
     return group;
   } catch (error) {
@@ -58,6 +92,7 @@ export const deleteGroup = async (id) => {
     if (!existanceGroup) {
       throw { status: 404, message: `Groupe avec l'ID ${id} introuvable` };
     }
+    await userGroupRepository.deleteUserGroupByGroupId(id);
     const group = await groupRepository.deleteGroup(id);
     return group;
   } catch (error) {
